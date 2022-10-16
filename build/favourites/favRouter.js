6a0e434d5eea75255ff48b197b29c268b3f6adc6
JonFerri <jftonymm@gmail.com>
2022-10-16 21:25:14 +0200

    new version

--- build/favourites/favRouter.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import favControllers, { authenticateToken } from "./favControllers.js";
-const favRouter = Router();
-favRouter.post("/createFav", authenticateToken, favControllers.createFav);
-favRouter.get("/getFavs", authenticateToken, favControllers.getFavs);
-favRouter.delete("/deleteAllFavs", favControllers.deleteAllFavs);
-favRouter.delete("/deleteUserFav/:id", authenticateToken, favControllers.deleteFav);
-export default favRouter;
